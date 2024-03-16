c2f2ff16b60be27b01833c40ddf83e76e927d8c6
yuuki.kato <0me3210g039530z@gmail.com>
2024-03-17 07:05:30 +0900

    add sync python. modify async python

--- sync_http_request.py
+++ sync_http_request.py
@@ -1,21 +1,21 @@
 import datetime
-import aiohttp
-import asyncio
+import requests
 
 start = datetime.datetime.now()
 
 def log(message):
+    """ログを出力する関数"""
     print(f'{(datetime.datetime.now() - start).seconds}秒経過', message)
 
-async def fetch(session, url):
-    """非同期にURLからデータを取得する関数"""
+def fetch(url):
+    """同期的にURLからデータを取得する関数"""
     print(f"Fetching {url}")
-    async with session.get(url) as response:
-        return await response.text()
+    response = requests.get(url)
+    return response.text
 
-async def main():
+def main():
+    """メインの同期処理を行う関数"""
     log("タスク開始")
-    """メインの非同期処理を行う関数"""
     urls = [
         "http://google.com",
         "http://qiita.com",
@@ -37,22 +37,15 @@ async def main():
         "https://nodejs.org/en"
     ]
     
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch(session, url) for url in urls]
-        
-        print("Starting tasks...")
-        # 非同期タスクを開始する前にメッセージを出力
-        print("Tasks are running in the background...")
-        
-        # 非同期タスクの結果を待つ
-        results = await asyncio.gather(*tasks)
-        
-        print("Tasks completed. Results:")
-        for result in results:
-            print(result[:100])  # 結果の最初の100文字を表示
+    print("Starting tasks...")
+    # 同期的にHTTPリクエストを実行
+    results = [fetch(url) for url in urls]
+    
+    print("Tasks completed. Results:")
+    for result in results:
+        print(result[:100])  # 結果の最初の100文字を表示
     
     log("タスク終了")
 
-# asyncio.run()を使ってメイン関数を実行する
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
