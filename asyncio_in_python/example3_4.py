f8912c85a9b68bdc4556b5dd54db50cb40cc802a
yuuki.kato <0me3210g039530z@gmail.com>
2024-03-29 09:02:39 +0900

    modify ex 3_4

--- asyncio_in_python/example3_4.py
+++ asyncio_in_python/example3_4.py
@@ -4,5 +4,11 @@ import inspect
 async def f():
   return 123
 
+def g():
+  return 456
+
 print(type(f))
+print(type(g))
+
 print(inspect.iscoroutinefunction(f))
+print(inspect.iscoroutinefunction(g))
